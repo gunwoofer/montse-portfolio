f56a51660659ae67e57c6b56b013e04d4a81649b
Valentin Bouis <valentin.bouis@gmail.com>
2026-04-18 13:22:32 -0400

    Cleanup temporary project files and maintenance scripts

--- fix.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const f = '/home/non0/montse-portfolio/angular.json';
-const d = JSON.parse(fs.readFileSync(f));
-d.projects['montse-portfolio'].architect.build.options.assets.push({'glob':'**/*','input':'src/assets'});
-fs.writeFileSync(f, JSON.stringify(d, null, 2));
